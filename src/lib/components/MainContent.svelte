9c692a4bf024ade02340610820a344958a678aee
louispawaon <lpawaon@gmail.com>
2026-03-17 21:35:31 +0800

    feat: main content for homepage

---
+++ src/lib/components/MainContent.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import IntroSection from '$lib/components/IntroSection.svelte';
+	import ExperienceSection from '$lib/components/ExperienceSection.svelte';
+	import type { HomepageContent } from '../../routes/+page';
+	import type { ExperienceEntryFormatted } from '$lib/utils/experience';
+
+	let {
+		homepage,
+		experiences
+	}: {
+		homepage: HomepageContent;
+		experiences: ExperienceEntryFormatted[];
+	} = $props();
+</script>
+
+<div class="flex flex-col">
+	<IntroSection
+		name={homepage.name}
+		introLine={homepage.introLine}
+		description={homepage.description}
+		ctaText={homepage.ctaText}
+		ctaHref={homepage.ctaHref}
+	/>
+
+	<div class="mt-[135px]">
+		<ExperienceSection
+			{experiences}
+			ctaText={homepage.experienceCtaText}
+			ctaHref={homepage.experienceCtaHref}
+		/>
+	</div>
+</div>
