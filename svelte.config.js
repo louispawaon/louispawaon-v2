c4125ac92aa0ee32afe1280d2e7b540b5254f7c3
louispawaon <lpawaon@gmail.com>
2026-03-22 10:19:47 +0800

    refactor: align to cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,23 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
+	}
+};
 
 export default config;
