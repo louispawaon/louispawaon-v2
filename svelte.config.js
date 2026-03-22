01dc02d35f0dcb09869d9358ac32915a0396fcc9
louispawaon <lpawaon@gmail.com>
2026-03-22 10:27:23 +0800

    chore: refactor svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,6 @@ const config = {
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
 			}
 		})
 	}
